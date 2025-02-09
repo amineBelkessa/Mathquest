d71560c82c882bf39a14e4690b02612c5b64f77a
Zabili <zakaria782001@outlook.fr>
2025-02-09 18:43:35 +0100

    Correction numero 4 des erreurs CI/CD : backend

--- src/test/java/org/example/mathquest/MathQuestApplicationTests.java
+++ src/test/java/org/example/mathquest/MathQuestApplicationTests.java
@@ -1,9 +1,10 @@
 package org.example.mathquest;
 
+import com.example.demo.MathQuestApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MathQuestApplication.class)
 class MathQuestApplicationTests {
 
     @Test
