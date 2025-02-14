d00e9b447e168b3d1462705f67bc93bc22379737
Zabili <zakaria782001@outlook.fr>
2025-02-14 10:44:34 +0100

    Sauvegarde avant récupération de dev

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import RegisterForm from "./components/Auth/RegisterForm";
+import Login from "./components/Auth/Login";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 
@@ -13,11 +14,13 @@ function App() {
             <nav>
                 <Link to="/">Accueil</Link>
                 <Link to="/register">Inscription</Link>
+                <Link to="/Login">Connexion</Link>
             </nav>
 
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/register" element={<RegisterForm />} />
+                <Route path="/login" element={<Login />} />
 
             </Routes>
         </Router>
