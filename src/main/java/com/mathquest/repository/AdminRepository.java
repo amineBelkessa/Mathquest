bb633296d30c2e6e5e4729c20a9426ed3de492ec
Zabili <zakaria782001@outlook.fr>
2025-03-16 14:56:03 +0100

    ajout du backend admin

---
+++ src/main/java/com/mathquest/repository/AdminRepository.java
@@ -0,0 +1,9 @@
+package com.mathquest.repository;
+
+import com.mathquest.model.Admin;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import java.util.Optional;
+
+public interface AdminRepository extends MongoRepository<Admin, String> {
+    Optional<Admin> findByEmail(String email);
+}
