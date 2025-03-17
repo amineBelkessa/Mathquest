febbfb67509754917ff865edcbb6d17d7975df68
Zabili <zakaria782001@outlook.fr>
2025-03-17 14:02:24 +0100

    Gestion des utilisateur par l'admin version 1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
 // @ts-ignore
 import Header from "./components/Layout/Header.tsx";
 // @ts-ignore
@@ -10,15 +10,18 @@ import RegisterForm from "./components/Auth/RegisterForm.tsx";
 // @ts-ignore
 import Footer from "./components/Layout/Footer.tsx";
 import React from "react";
+import AdminUtilisateurs from "./pages/AdminUtilisateurs";
+import {getUser} from "./services/auth.service";
 
 function App() {
-
+    const user = getUser();
     return (
         <Router>
             <div className="flex flex-col min-h-screen">
                 <Header />
                 <main className="flex-grow container mx-auto p-4">
                     <Routes>
+                        <Route path="/admin/utilisateurs" element={user?.role === "admin" ? <AdminUtilisateurs /> : <Navigate to="/" />} />
                         <Route path="/" element={<Home />} />
                         <Route path="/register" element={<RegisterForm />} />
                         <Route path="/login" element={<Login />} />
