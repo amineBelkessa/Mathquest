145bbd115d611c90a275bf1d29edf21eca547fca
abderrahmane <hathoutyes2017@example.com>
2025-03-20 00:30:02 +0100

    le retour de SecurityConfig.java

--- src/main/java/com/mathquest/MathQuestApplication.java
+++ src/main/java/com/mathquest/MathQuestApplication.java
@@ -1,10 +1,8 @@
 package com.mathquest;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+
 public class MathQuestApplication {
 
     public static void main(String[] args) {
