fe3aff07237952f2c7b27667e87ecbc930b7bbd1
Zabili <zakaria782001@outlook.fr>
2025-03-20 01:02:07 +0100

    modification sécurité

--- src/main/java/com/mathquest/config/SecurityConfig.java
+++ src/main/java/com/mathquest/config/SecurityConfig.java
@@ -11,12 +11,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Disable CSRF protection
-                .cors(cors -> cors.disable()) // Disable CORS (handled by Nginx)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/**").permitAll() // Allow all requests
-                );
 
+                .csrf(csrf -> csrf.disable()) // Désactiver CSRF
+                .cors(cors -> cors.disable()) // Désactiver CORS
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll()) // Autoriser toutes les requêtes
+                .httpBasic(httpBasic -> httpBasic.disable()) // Désactiver l'authentification basique
+                .formLogin(form -> form.disable());  // Désactive Form Login
         return http.build();
     }
 }
+
+
+
