5dd6030bfc6870913eaeae36e64cc005702d2b36
moussma8 <mahad.moussa-abdillahi@univ-rouen.fr>
2025-04-20 16:20:44 +0200

    pour merge

--- frontend/src/services/admin.service.ts
+++ frontend/src/services/admin.service.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/admin";
+const API_URL = "http://srv-dpi-proj-mathquest-test.univ-rouen.fr/api/admin";
 export const fetchEleves = async () => {
     const response = await axios.get(`${API_URL}/utilisateurs`);
     return response.data;
--- frontend/src/services/auth.service.ts
+++ frontend/src/services/auth.service.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api";
+const API_URL = "http://srv-dpi-proj-mathquest-test.univ-rouen.fr/api/";
 
 export async function register(username: string, email: string, password: string, role: string): Promise<string> {
     const response = await fetch(`${API_URL}/register`, {
