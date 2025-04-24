57fa0d28704654205ffd03e65af18177c22589f9
Abderrahmane EL HATHOUT <abderrahmane.el-hathout@univ-rouen.fr>
2025-04-24 02:35:43 +0200

    Update auth.service.ts

--- frontend/src/services/auth.service.ts
+++ frontend/src/services/auth.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 // http://localhost:8080/api   local
 //http://srv-dpi-proj-mathquest-test.univ-rouen.fr/api  sur le serveur
-const API_URL = "http://srv-dpi-proj-mathquest-test.univ-rouen.fr/api ";
+const API_URL = "http://srv-dpi-proj-mathquest-test.univ-rouen.fr/api";
 
 export async function register(username: string, email: string, password: string, role: string): Promise<string> {
     const response = await fetch(`${API_URL}/register`, {
