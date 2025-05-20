6d1f0be8bb8049b6bfe68e2af2dd0af5f194cd4b
moussma8 <mahad.moussa-abdillahi@univ-rouen.fr>
2025-05-20 19:01:46 +0200

    correction url test

--- frontend/src/services/admin.service.ts
+++ frontend/src/services/admin.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 // http://localhost:8080/api/admin   local
 //http://srv-dpi-proj-mathquest-test.univ-rouen.fr/api/admin   sur le serveur
-const API_URL = "http://srv-dpi-proj-mathquest-testS.univ-rouen.fr/api/admin";
+const API_URL = "http://srv-dpi-proj-mathquest-test.univ-rouen.fr/api/admin";
 
 export const fetchEleves = async () => {
     const response = await axios.get(`${API_URL}/utilisateurs`);
